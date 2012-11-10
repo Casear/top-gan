f458444f827198b453de9b98943fd2d3c1a33860
snoopy5k <snoopy5k@msn.com>
2012-11-10 11:19:01 +0800

    add socket method

--- public/scripts/init.js
+++ public/scripts/init.js
@@ -1,11 +1,3 @@
 window.onload = function() {
 	initialize_map();
-
-	// $.each(json, function(i, v){
-
-	// });
-
-	for (var i = 50 - 1; i >= 0; i--) {
-		setMarker([{"n":"name_"+i, "x": i+100, "y": i*20}]);
-	};
 }
--- public/scripts/socket.js
+++ public/scripts/socket.js
@@ -4,11 +4,16 @@ socket = io.connect();
 socket.on('connect', function() {
     console.log('Client has connected to the server!');
 });
-
-socket.on('system', function(data) {
-    console.log(data);
-});
-
 socket.on('disconnect', function() {
     console.log('The client has disconnected!');
 });
+
+socket.on('war', function(data) {
+	console.log('socket.on war');
+	setMarker(data);
+});
+
+function fly() {
+	console.log('call socket.emit fly, x:' + user.x + ', y:' + user.y);
+	socket.emit('fly', { x: user.x, y: user.y });
+}
