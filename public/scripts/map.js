5fbecc1033c90ad55a9be5f6b4c07d02f65b996e
Wright Liu <wrightliumail@gmail.com>
2012-11-10 14:06:18 +0800

    update

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,7 +1,8 @@
 var map;
 var markersArray = [];
+user = { "n":"user", "x":25.139636, "y":121.495840, "r":0 };
 function initialize_map() {
-	var latlng = new google.maps.LatLng(34.3, 150.6);
+	var latlng = new google.maps.LatLng(user.x, user.y);
 	var myOptions = {
 	  zoom: 8,
 	  center: latlng,
@@ -15,7 +16,7 @@ function initialize_map() {
 		map.setCenter(newlatlng);
 		setMarker([{"n":"n","x":newlatlng.lat(),"y":newlatlng.lng() }]);
 		fly(map.center.lat() , map.center.lng())
-		},1000);
+		},250);
 
 	// setMarker([{"n":"n","x":34.3,"y":150.6}]);
 	// setMarker([{"n":"n","x":35.3,"y":151.6},{"n":"n1","x":36.397,"y":152.644}]);
