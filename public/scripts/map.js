943b332d64dce6d9a89b752bd741b4bc057d5abc
snoopy5k <snoopy5k@msn.com>
2012-11-10 09:33:24 +0800

    modify map ref

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,1 +1,12 @@
-var map;
+function initialize() {
+	var latlng = new google.maps.LatLng(-34.397, 150.644);
+	var myOptions = {
+	  zoom: 8,
+	  center: latlng,
+	  mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	var map = new google.maps.Map(document.getElementById("map_canvas"),
+	    myOptions);
+}
+
+window.onload = initialize; 
