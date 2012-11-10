a0ced3a5d98e25b5c9c0f3350f7b46eab51ff861
Wright Liu <wrightliumail@gmail.com>
2012-11-10 11:30:07 +0800

    remove padding

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,3 +1,5 @@
+var map;
+var markersArray = [];
 function initialize_map() {
 	var latlng = new google.maps.LatLng(-34.397, 150.644);
 	var myOptions = {
@@ -5,18 +7,35 @@ function initialize_map() {
 	  center: latlng,
 	  mapTypeId: google.maps.MapTypeId.SATELLITE
 	};
-	var map = new google.maps.Map(document.getElementById("map_canvas"),
+	map = new google.maps.Map(document.getElementById("map_canvas"),
 	    myOptions);
 }
-var markersArray = [];
-
 function setMarker (json)
 {
-	//if(markersArray.length > 0){
-	//	for(int i = 0 ; i < markersArray.length ; i++) 
-	//	{
-	//		markersArray[i].setMap(null);
-	//	}
-	//}
-	
+	clearOverlays();
+	$.each(json, function(i,v)
+	{
+		var myLatLng = new google.maps.LatLng( v.x, v.y);
+		var image = new google.maps.MarkerImage(
+						'bluedot_retina.png',
+						null, // size
+						null, // origin
+						new google.maps.Point( 8, 8 ), // anchor (move to center of marker)
+						new google.maps.Size( 17, 17 ) // scaled size (required for Retina display icon)
+					);
+		markersArray[i] = new google.maps.Marker({
+						flat: true,
+						icon: image,
+						map: map,
+						optimized: false,
+						position: myLatLng,
+						title: 'I might be here',
+						visible: true
+					});
+	})
+}
+function clearOverlays() {
+	$.each(markersArray, function(i, v){
+		markersArray[i].setMap(null);
+	});
 }
