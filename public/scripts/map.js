a663ed3b6cfd2d5b718d7f134a8ae0ed15d4bf14
Wright Liu <wrightliumail@gmail.com>
2012-11-10 13:42:12 +0800

    update

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,7 +1,7 @@
 var map;
 var markersArray = [];
 function initialize_map() {
-	var latlng = new google.maps.LatLng(-34.397, 150.644);
+	var latlng = new google.maps.LatLng(34.3, 150.6);
 	var myOptions = {
 	  zoom: 8,
 	  center: latlng,
@@ -9,6 +9,16 @@ function initialize_map() {
 	};
 	map = new google.maps.Map(document.getElementById("map_canvas"),
 	    myOptions);
+
+	self.setInterval(function(){
+		var newlatlng = new google.maps.LatLng(map.center.lat()+0.1, map.center.lng());
+		map.setCenter(newlatlng);
+		setMarker([{"n":"n","x":newlatlng.lat(),"y":newlatlng.lng() }]);
+		fly(map.center.lat() , map.center.lng())
+		},1000);
+
+	// setMarker([{"n":"n","x":34.3,"y":150.6}]);
+	// setMarker([{"n":"n","x":35.3,"y":151.6},{"n":"n1","x":36.397,"y":152.644}]);
 }
 function setMarker (json)
 {
@@ -17,7 +27,7 @@ function setMarker (json)
 	{
 		var myLatLng = new google.maps.LatLng( v.x, v.y);
 		var image = new google.maps.MarkerImage(
-						'bluedot_retina.png',
+						'images/F35-JSF-48px.png',
 						null, // size
 						null, // origin
 						new google.maps.Point( 8, 8 ), // anchor (move to center of marker)
@@ -29,7 +39,7 @@ function setMarker (json)
 						map: map,
 						optimized: false,
 						position: myLatLng,
-						title: 'I might be here',
+						title: v.n,
 						visible: true
 					});
 	})
