ebd648489ac4879753e7678a5f363ab2fd3d45a2
Wright Liu <wrightliumail@gmail.com>
2012-11-11 18:06:29 +0800

    Fixed Bug

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -259,22 +259,6 @@ function setBomb( json)
 	{
 		if(json.name == v.getTitle())
 		{
-			var image = new google.maps.MarkerImage(
-				'images/bomb1.png',
-				null, // size
-				null, // origin
-				new google.maps.Point( 8, 8 ), // anchor (move to center of marker)
-				new google.maps.Size( 50, 50 ) // scaled size (required for Retina display icon)
-			);
-			arrayMissle[arrayMissle.length] = new google.maps.Marker({
-				flat: true,
-				icon: image,
-				map: map,
-				optimized: false,
-				position: vLatLng,
-				title: json.name,
-				visible: true
-			});
 			v.setPosition(vLatLng);
 			IsExist = 1;
 		}
